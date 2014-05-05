f17a50177c9382b5b796b41277897367000c4455
Brad <brad@poltergasm>
2014-05-05 16:09:22 +0100

    Updating

--- cor-www/js/mold.js
+++ cor-www/js/mold.js
@@ -1,21 +1,26 @@
 $(function() {
   /* Nav stuff */
   var ulNav = $('ul.nav'),
-      fade  = $('#fade'),
-      navToggle = $('a.nav-toggle');
+      navToggle = $('a.nav-toggle'),
+      fade      = $('#fade');
 
-  navToggle.click(function() { ulNav.toggle(); });
-  navToggle.blur(function() { ulNav.toggle(); });
+  navToggle.click(function() {
+    ulNav.toggle();
+  });
+
+  navToggle.blur(function() {
+    ulNav.toggle();
+  });
   
-  /* Modals */
   $('a[data-toggle="modal"]').click(function() {
     var modal = $($(this).attr('data-modal'));
     modal.show('fade');
+    modal.addClass('modal-open');
     fade.show();
   });
   
   $('.close-modal').click(function() {
     fade.hide();
-    $('.modal').hide();
+    $('.modal-open').hide('fade');
   });
 });
