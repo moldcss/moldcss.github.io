ea2a3a3afa9d311ed5bbca97d6635a807d4aaf75
Brad Haywood <brad.haywood@opusvl.com>
2014-08-19 11:14:22 +0100

    blah

--- pano/jspanoviewer.js
+++ pano/jspanoviewer.js
@@ -392,12 +392,12 @@ JSPanoViewer.prototype = {
 
         controller.addEventListener('touchmove', function(event) {
             if(typeof(event) == 'undefined') event = window.event;
-            if(controller.touchStart) {
-                var degrees = (controller.lastX - event.touches[0].clientX / 10);
+            //if(controller.touchStart) {
+                var degrees = (controller.lastX - event.touches[0].clientX / 5);
                 self.shiftPano(degrees);
                 controller.lastX = event.touches[0].clientX;
                 controller.lastY = event.touches[0].clientY;
-            }
+            //}
             return false; // Prevent default behaviour
         });
 	},
